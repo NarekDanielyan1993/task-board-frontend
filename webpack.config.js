445edb0fae45c958861014f3d2f19e1b81dd83c2
NarekDanielyan1993 <danielyann729@gmail.com>
2024-03-26 21:32:31 +0400

    dsad

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './public/index.html',
         }),
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].js',
